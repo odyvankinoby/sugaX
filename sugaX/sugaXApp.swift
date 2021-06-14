378d1b4485c031dc579846774aefa4c60c1618cf
Nicolas Ott <nicolasott@gmail.com>
2021-06-14 12:57:42 +0200

    Initial Commit

---
+++ sugaX/sugaXApp.swift
@@ -0,0 +1,17 @@
+//
+//  sugaXApp.swift
+//  sugaX
+//
+//  Created by Nicolas Ott on 14.06.21.
+//
+
+import SwiftUI
+
+@main
+struct sugaXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
